03a48bde4e115075b825ec12c319de69fa681aa0
djeisonCassimiro <djeison.dev@gmail.com>
2021-07-13 08:34:56 -0300

    Implementação onLongClick

--- lib/pages/carro/carros_list_view.dart
+++ lib/pages/carro/carros_list_view.dart
@@ -4,17 +4,14 @@ import 'package:carros/pages/carro/carro_page.dart';
 import 'package:carros/util/nav.dart';
 import 'package:flutter/material.dart';
 
-
 // ignore: must_be_immutable
 class CarrosListView extends StatelessWidget {
   List<Carro> carros;
-
 
   CarrosListView(this.carros);
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
       padding: EdgeInsets.all(16),
       child: ListView.builder(
@@ -22,48 +19,57 @@ class CarrosListView extends StatelessWidget {
         itemBuilder: (context, index) {
           Carro c = carros[index];
 
-          return Card(
-            color: Colors.grey[100],
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: CachedNetworkImage(
-                      imageUrl: c.urlFoto ??
-                          "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/luxo/Shelby_Supercars_Ultimate.png",
-                      width: 250,
-                    )
-                  ),
-                  Text(
-                    c.nome ?? "",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  Text(
-                    "descrição...",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  ButtonBarTheme(
-                    data: ButtonBarThemeData(),
-                    child: ButtonBar(
-                      children: <Widget>[
-                        TextButton(
-                          child: const Text('DETALHES'),
-                          onPressed: () => _onClickDetalheCarro(context, c),
+          return Container(
+            height: 280,
+            child: InkWell(
+              onTap: () {
+                _onClickDetalheCarro(context, c);
+              },
+              onLongPress: () {
+                _onLongClickDetalheCarro(context, c);
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Center(
+                          child: CachedNetworkImage(
+                        imageUrl: c.urlFoto ??
+                            "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/luxo/Shelby_Supercars_Ultimate.png",
+                        width: 250,
+                      )),
+                      Text(
+                        c.nome ?? "",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Text(
+                        "descrição...",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      ButtonBarTheme(
+                        data: ButtonBarThemeData(),
+                        child: ButtonBar(
+                          children: <Widget>[
+                            TextButton(
+                              child: const Text('DETALHES'),
+                              onPressed: () => _onClickDetalheCarro(context, c),
+                            ),
+                            TextButton(
+                              child: const Text('SHARE'),
+                              onPressed: () {
+                                _onClickSharedCarro(context, c);
+                              },
+                            ),
+                          ],
                         ),
-                        TextButton(
-                          child: const Text('SHARE'),
-                          onPressed: () {
-                            /* ... */
-                          },
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           );
@@ -74,5 +80,35 @@ class CarrosListView extends StatelessWidget {
 
   _onClickDetalheCarro(context, Carro c) {
     push(context, CarroPage(c));
+  }
+
+  void _onLongClickDetalheCarro(BuildContext context, Carro c) {
+    showDialog(context: context, builder: (context) {
+      return SimpleDialog(
+        title: Text(c.nome),
+        children: <Widget>[
+          ListTile(
+            title: Text("Detalhes"),
+            leading: Icon(Icons.directions_car),
+            onTap: () {
+              pop(context);
+              _onClickDetalheCarro(context, c);
+            },
+          ),
+          ListTile(
+            title: Text("Share"),
+            leading: Icon(Icons.share),
+            onTap: () {
+              pop(context);
+              _onClickSharedCarro(context, c);
+            },
+          ),
+        ],
+      );
+    });
+  }
+
+  void _onClickSharedCarro(BuildContext context, Carro c) {
+    print("Share ${c.nome}");
   }
 }
